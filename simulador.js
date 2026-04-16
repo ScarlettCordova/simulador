ca56887d7f279530b549d4f3d313060668156d82
ScarlettCordova <scarargecorb@gmail.com>
2026-04-15 21:55:09 -0500

    se creó funciones calcularDisponible, función calcular, calcularCapacidadPago y se llamó a la función calcularPago desde función calcular

--- simulador.js
+++ simulador.js
@@ -1,1 +1,22 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    let ingresosFloat=0;
+    let egresosFloat=0;
+    let cmpIngresosFloat;
+    let cmpEgresosFloat;
+    let valorDisponibleFloat;
+    let total;
+    let monto;
+    let capacidadPago;
+    cmpIngresosFloat=document.getElementById("txtIngresos");
+    cmpEgresosFloat=document.getElementById("txtEgresos");
+    ingresosFloat=parseFloat(cmpIngresosFloat.value);
+    egresosFloat=parseFloat(cmpEgresosFloat.value);
+    valorDisponibleFloat=calcularDisponible(ingresosFloat,egresosFloat);
+    total=document.getElementById("spnDisponible");
+    total.innerText=valorDisponibleFloat;
+    monto=calcularCapacidadPago(valorDisponibleFloat);
+    capacidadPago=document.getElementById("spnCapacidadPago");
+    capacidadPago.innerText=monto;
+
+}
